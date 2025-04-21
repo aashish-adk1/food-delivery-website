63499ec18e5bb147e8aadf4a5a949e40640053e7
Aashish Adhikari <aashish.adk156@gmail.com>
2025-04-21 19:16:10 +0545

    Final

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import SearchProvider from './context/searchProvider.jsx'
+import SearchProvider from './context/SearchProvider.jsx'
 createRoot(document.getElementById('root')).render(
   <SearchProvider>
     <App />
